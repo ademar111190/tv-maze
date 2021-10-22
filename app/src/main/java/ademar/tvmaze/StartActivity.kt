b502a08925faf54b54e1a75b87446ec1dd269ea8
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-10-22 19:49:37 -0300

    Set up app entry point

--- app/src/main/java/ademar/tvmaze/StartActivity.kt
+++ app/src/main/java/ademar/tvmaze/StartActivity.kt
@@ -1,13 +1,16 @@
 package ademar.tvmaze
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
 }
