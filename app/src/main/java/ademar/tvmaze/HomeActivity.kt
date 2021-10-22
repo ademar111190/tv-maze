bbd7afa32da62c3f15dec127ae768fb367662100
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-10-22 20:15:39 -0300

    Add shortcut entry point to series

--- app/src/main/java/ademar/tvmaze/HomeActivity.kt
+++ app/src/main/java/ademar/tvmaze/HomeActivity.kt
@@ -2,12 +2,44 @@ package ademar.tvmaze
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import timber.log.Timber
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_home)
+        setUpNavigation()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (supportFragmentManager.fragments.isEmpty()) {
+            finish()
+        }
+    }
+
+    private fun setUpNavigation() {
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigation.setOnItemReselectedListener { menu ->
+            Timber.d("menu reselected $menu") // TODO
+        }
+        navigation.setOnItemSelectedListener { menu ->
+            Timber.d("menu selected $menu") // TODO
+            false
+        }
+        navigation.selectedItemId = R.id.navigation_series
+        supportFragmentManager.addOnBackStackChangedListener {
+            Timber.d("back stack changed listener") // TODO
+        }
+        val initial = when (intent?.extras?.getString("INITIAL_ACTION", null)) {
+            "ademar.tvmaze.series" -> R.id.navigation_series
+            else -> null
+        }
+        if (initial != null) {
+            navigation.selectedItemId = initial
+        }
     }
 
 }
--- app/src/main/java/ademar/tvmaze/StartActivity.kt
+++ app/src/main/java/ademar/tvmaze/StartActivity.kt
@@ -8,7 +8,9 @@ class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val action = intent?.action
         val intent = Intent(this, HomeActivity::class.java)
+        intent.putExtra("INITIAL_ACTION", action)
         startActivity(intent)
         finish()
     }
