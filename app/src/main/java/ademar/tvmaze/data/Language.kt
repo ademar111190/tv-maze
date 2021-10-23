21deb0a76c452a9ce50609fdbce3d93c4e381f04
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-10-22 22:21:59 -0300

    Add language and genre icons

--- app/src/main/java/ademar/tvmaze/data/Language.kt
+++ app/src/main/java/ademar/tvmaze/data/Language.kt
@@ -1,8 +1,14 @@
 package ademar.tvmaze.data
 
-enum class Language {
-    ENGLISH,
-    PORTUGUESE,
-    JAPANESE,
-    OTHER,
+import ademar.tvmaze.R
+import androidx.annotation.DrawableRes
+
+enum class Language(
+    @DrawableRes val icon: Int,
+) {
+    ENGLISH(R.drawable.lang_english),
+    JAPANESE(R.drawable.lang_japanese),
+    PORTUGUESE(R.drawable.lang_portuguese),
+    SPANISH(R.drawable.lang_spanish),
+    OTHER(R.drawable.lang_other),
 }
