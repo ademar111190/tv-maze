b76c7c7a111098c1a2b599f925f4600bfdd1dcdb
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-10-22 21:42:36 -0300

    Add arch structure

--- app/src/main/java/ademar/tvmaze/App.kt
+++ app/src/main/java/ademar/tvmaze/App.kt
@@ -1,11 +1,16 @@
 package ademar.tvmaze
 
+import ademar.tvmaze.arch.ArchBinder
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
+
+    // creates arch binder before any activity
+    @Inject lateinit var archBinder: ArchBinder
 
     override fun onCreate() {
         super.onCreate()
---
+++ app/src/main/java/ademar/tvmaze/arch/ArchErrorMapper.kt
@@ -0,0 +1,23 @@
+package ademar.tvmaze.arch
+
+import io.reactivex.rxjava3.core.Observable
+
+interface ArchErrorMapper<State : Any> {
+
+    fun mapError(error: Throwable): Observable<State>
+
+    class Impl<State : Any>(
+        private val stateFactory: (String) -> State,
+    ) : ArchErrorMapper<State> {
+
+        override fun mapError(error: Throwable): Observable<State> {
+            return Observable.just(
+                stateFactory(
+                    error.localizedMessage ?: error.message ?: "$error",
+                ),
+            )
+        }
+
+    }
+
+}
